9daf7cb859ddfc9ce6ae8864ac5a8cec73627b6a
Derrick <derrickrunger@gmail.com>
2020-02-11 17:44:40 -0800

    First Commit

---
+++ Homework5/HW5_24_skoll_P1.py
@@ -0,0 +1,30 @@
+"""
+Homework 5
+>Problem 1
+
+Author: Derrick Unger
+Date: 2/14/20
+CSC232 Winter 2020
+"""
+
+import numpy as np
+
+# User Input
+while True:
+    try:
+        seed = int(input("\nInput random generator seed: "))
+
+        if seed < 0:
+            print("Seed must be greater or equal to 0!")
+        elif seed > 2**32 - 1:
+            print("Seed too large. Must be < 2**32 - 1")
+        else:
+            np.random.seed(seed)
+            randValue = np.random.randint(low=-8, high=9, size=15)
+            break
+
+    except:
+        print("\nError: Invalid input, retry\n")
+
+
+print("Result:", randValue)
