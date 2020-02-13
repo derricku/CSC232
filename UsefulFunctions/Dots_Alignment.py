9b0bec2f6bffc464e6a95efa970795606098627c
Derrick <derrickrunger@gmail.com>
2020-02-13 13:14:19 -0800

    Added Dots Alignment to useful functions

---
+++ UsefulFunctions/Dots_Alignment.py
@@ -0,0 +1,50 @@
+"""
+Dots Alignment Algorithm
+
+Author: Derrick Unger
+Date: 2/14/20
+CSC232 Winter 2020
+"""
+L = ["string", -12.3456, 100, "more Strings!", 212312.2222, -0.0, 5]
+L1 = []
+d = 0
+
+# Removes Strings
+for x in L:
+    try:
+        d = str(x)
+        decimalPlace = d.find(".")
+        if decimalPlace == -1:
+            try:
+                add = int(x)
+                addd = str(add)
+                L1.append(addd)
+            except:
+                print("String has been removed.")
+        if decimalPlace > 0:
+            add = float(x)
+            addd = str(add)
+            L1.append(addd)
+    except:
+        print("String has been removed")
+
+
+# Align Dots
+a = 0
+b = 0
+for x in L1:
+    decimalPlace = x.find(".")
+    if decimalPlace > a:
+        a = decimalPlace
+    if decimalPlace == -1:
+        if len(x) > a:
+            a = len(x)
+c = 0
+
+for x in L1:
+    decimalPlace = x.find(".")
+    if decimalPlace == -1:
+        spaces = a - len(x)
+    else:
+        spaces = a - decimalPlace
+    print(spaces*" ", x)
