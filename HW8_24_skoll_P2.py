7bd089260236f969103fb1d021d64770a8ffd2e1
DerrickU <derrickrunger@gmail.com>
2020-03-03 15:06:46 -0800

    Error in line 59

---
+++ HW8_24_skoll_P2.py
@@ -0,0 +1,72 @@
+"""
+Homework 8
+>Problem 2
+
+Author: Derrick Unger
+Date: 3/7/20
+
+CSC232 Winter 2020
+"""
+import datetime
+
+# User Input Date
+while True:
+    try:
+        Date = input("\nInput date in format 2/13/2020: ")
+        
+        # Test for Valid Date and Assign Date Var
+        month, day, year = Date.split("/")
+        date = datetime.date(int(year), int(month), int(day))
+        print(month, day, year)
+        # myDay = date.strftime("%a")
+
+        break
+    except:
+        print("\nError: Invalid date, retry\n")
+        
+
+# Read in .csv
+f = open("PATIENT.csv", "r")
+DOBorig = []
+
+n = 0  #Skip Column Headers
+for line in f:
+    line = line.strip()
+    if n == 0:
+        HEADER = line
+    if n != 0:
+        PID, lastname, firstname, gender, DOB,\
+            BloodType, Insurance, dID = line.split(",")
+            
+        month3, day3, year3 = DOB.split("/")
+        date3 = datetime.date(int(year3), int(month3), int(day3))
+        
+        DOBorig.append((PID, lastname, firstname, gender, date3,
+                    BloodType, Insurance, dID))
+    n += 1
+
+
+# Remove DOB Values
+DOBremove = []
+for x in range(len(DOBorig)):
+    if DOBorig[x][4] > date:
+        DOBremove.append(DOBorig[x])
+    else:
+        pass
+
+# Sort .csv
+DOBsort = DOBremove
+DOBsort.sort(key=lambda date: DOBsort[4])
+f.close()
+
+print(HEADER + "\n")
+f = open("sorted.txt", "w")
+f.write(HEADER+ "\n")
+for PID, lastname, firstname, gender, DOB, BloodType, Insurance, dID in DOBsort:
+    orderedDOB = str(DOB) + ", " + PID + ", " + lastname + ", " + firstname + ", "\
+        + gender + ", " + dID + ", " + BloodType + ", " + Insurance + "\n"
+ 
+    print(orderedDOB)
+    f.write(orderedDOB)
+
+f.close()
